c755f011a6b6e8718e43f10d9e15c751b018e2b7
Elijah Itah and Jayam Subramanyam <pair+elijah+jayam@allstate.com>
2016-06-22 17:00:35 -0500

    end of day

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,19 +1,35 @@
-/* eslint-disable new-cap */
+/* eslint-disable new-cap, array-callback-return, no-param-reassign */
 
 import express from 'express';
+import Priority from '../models/priority';
+import Category from '../models/category';
+import Task from '../models/task';
+import ViewTask from '../models/viewTask';
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('tasks/index');
+  Task.find((err, tasksArray) => {
+    console.log('viewArray', tasksArray);
+    const priorities = Priority.find();
+    const viewTasks = tasksArray.map(t => new ViewTask(t, priorities));
+    console.log('viewtasks', viewTasks);
+    res.render('tasks/index', { viewTasks });
+  });
 });
 
 router.get('/new', (req, res) => {
-  res.render('tasks/new');
+  const categories = Category.find();
+  const priorities = Priority.find();
+  res.render('tasks/new', { categories, priorities });
 });
 
 /* create new one */
 router.post('/', (req, res) => {
-  res.redirect('/tasks');
+  console.log('the object is', req);
+  const myTask = new Task(req.body);
+  myTask.save(() => {
+    res.redirect('/tasks');
+  });
 });
 
 router.post('/:id/complete', (req, res) => {
