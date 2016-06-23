dca2eef8d279b0fbabbe034635d3c8a81f76170d
Elijah Itah <elijahbit@gmail.com>
2016-06-23 08:54:59 -0500

    Adding delete button

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,18 +1,20 @@
-/* eslint-disable new-cap, array-callback-return, no-param-reassign */
+/* eslint-disable new-cap, array-callback-return, no-param-reassign,
+ no-underscore-dangle */
 
 import express from 'express';
 import Priority from '../models/priority';
 import Category from '../models/category';
 import Task from '../models/task';
 import ViewTask from '../models/viewTask';
+// import ObjectID from 'mongodb';
 const router = module.exports = express.Router();
 
 router.get('/', (req, res) => {
   Task.find((err, tasksArray) => {
-    console.log('viewArray', tasksArray);
+    console.log('startmg find length of', tasksArray.length);
     const priorities = Priority.find();
     const viewTasks = tasksArray.map(t => new ViewTask(t, priorities));
-    console.log('viewtasks', viewTasks);
+    console.log('viewtasks worked');
     res.render('tasks/index', { viewTasks });
   });
 });
@@ -36,8 +38,15 @@ router.post('/:id/complete', (req, res) => {
   res.redirect('/tasks');
 });
 
+/* delete */
 router.post('/:id/delete', (req, res) => {
-  res.redirect('/tasks');
+  Task.findOne({ _id: req.params.id }, (err1, f) => {
+    if (err1) console.log('entering the delete', err1.message);
+    Task.remove({ _id: f._id }, (err2) => {
+      if (err2) console.log('entering the delete', err2.message);
+      res.redirect('/tasks');
+    });
+  });
 });
 
 router.get('/:id/edit', (req, res) => {
--- src/models/viewTask.js
+++ src/models/viewTask.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+
 function ViewTask(task, priorities) {
+  this.id = task._id;
   this.name = task.name;
   this.due = task.due.toLocaleDateString();
   this.priority = priorities.find(p => p.value === task.priority);
+  if (task.isComplete) {
+    this.priority.color = 'white';
+  } else {
+    this.priority.color = task.priority.color;
+  }
   this.category = task.category;
   this.isComplete = task.isComplete;
   this.createdAt = task.createdAt.toLocaleDateString();
